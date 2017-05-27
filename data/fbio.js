318523b9f21d82ce30a7bbe3c0cbf5221785a5fc
Lenin Alevski <alevski@freeagentsoftware.com>
2017-05-27 17:44:27 -0500

    firebase methods for login, logout and register

---
+++ data/fbio.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyB4meRiDuw4GQbbAA7T5B8zF_z7UIqcKEU",
+    authDomain: "transportame-c5b3a.firebaseapp.com",
+    databaseURL: "https://transportame-c5b3a.firebaseio.com",
+    storageBucket: "transportame-c5b3a.appspot.com",
+});
+
+export const login = async (email, password) => {
+  return firebase.auth().signInWithEmailAndPassword(email, pass);
+};
+
+export const logout = async () => {
+  return firebase.auth().signOut();
+};
+
+export const register = async (email, password) => {
+  return firebase.auth().createUserWithEmailAndPassword(email, pass);
+};
